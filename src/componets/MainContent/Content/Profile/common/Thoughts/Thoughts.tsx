97aedea76c57d49595a80cbe4249141bc22d4e63
dd24 <dich140804@gmail.com>
2024-11-01 00:03:22 +0300

    dd24

--- src/componets/MainContent/Content/Profile/common/Thoughts/Thoughts.tsx
+++ src/componets/MainContent/Content/Profile/common/Thoughts/Thoughts.tsx
@@ -1,16 +1,30 @@
 import ThoughtElement from './ThoughtElement/ThoughtElement'
 import thoughts from './thoughts.module.css'
+import NoThoughts from './NoThoughts/NoThoughts'
 
-const Thoughts = () => {
+const Thoughts = (props: any) => {
+    const thoughtsElements = props.profileThoughts.map((thought: any, index: number) => (
+        <ThoughtElement key={index} myId={props.myId} thoughtData={thought} />
+    ))
+    let userId = props.params.user_id
+    if (!userId) userId = props.myId
     return (
         <div className={thoughts.container}>
             <div className={thoughts.header}>
                 <div className={thoughts.title}>Настенные мысли</div>
             </div>
             <ul className={thoughts.thoughts_list}>
-                <ThoughtElement />
-                <ThoughtElement />
+                {thoughtsElements.length !== 0 ? thoughtsElements : <NoThoughts />}
             </ul>
+            {thoughtsElements.length !== 0 && (
+                <footer className={thoughts.footer}>
+                    <button
+                        className={thoughts.show_more_btn}
+                        onClick={() => props.setThoughtsPage(userId, props.thoughtsPagesLoaded + 1, 3)}>
+                        Показать ещё...
+                    </button>
+                </footer>
+            )}
         </div>
     )
 }
--- src/redux/reducers/profileReducer.ts
+++ src/redux/reducers/profileReducer.ts
@@ -1,4 +1,4 @@
-import { subscribesAPI, usersAPI } from "../../API"
+import { subscribesAPI, thoughtsAPI, usersAPI } from "../../API"
 import { fallAuth, setAuthErrorText } from "./singInReducer"
 
 let initialProfileState = {
@@ -11,7 +11,9 @@ let initialProfileState = {
     isSubscribe: null,
     subscribeId: null,
     userSubscribes: [],
-    totalUserSubscribes: null
+    totalUserSubscribes: null,
+    profileThoughts: [],
+    thoughtsPagesLoaded: 0
 }
 
 const profileReducer = (state: any = initialProfileState, action: any) => {
@@ -26,29 +28,41 @@ const profileReducer = (state: any = initialProfileState, action: any) => {
                 aboutMe: action.userProfile.aboutMe,
                 isWallOpen: action.userProfile.isWallOpen
             }
-        case 'SET_SUBSCRIBE':
+        case 'SET-SUBSCRIBE':
             return {
                 ...state,
                 isSubscribe: action.subscribe.status,
                 subscribeId: action.subscribe.id ? action.subscribe.id : null
             }
-        case 'SUBSCRIBE': 
+        case 'SUBSCRIBE':
             return {
                 ...state,
                 isSubscribe: true,
                 subscribeId: action.subscribeId
             }
-        case 'UNSUBSCRIBE': 
+        case 'UNSUBSCRIBE':
             return {
                 ...state,
                 isSubscribe: false,
                 subscribeId: null
             }
-        case 'SET_USER_SUBSCRIBES':
+        case 'SET-USER-SUBSCRIBES':
             return {
                 ...state,
                 userSubscribes: action.subscribesPage,
                 totalUserSubscribes: action.totalUserSubscribes
+            }
+        case 'LOAD-FIRST-THOUGHTS-PAGE':
+            return {
+                ...state,
+                profileThoughts: action.thoughtsPage,
+                thoughtsPagesLoaded: 1
+            }
+        case 'SET-THOUGHTS-PAGE':
+            return {
+                ...state,
+                profileThoughts: [...state.profileThoughts, ...action.thoughtsPage],
+                thoughtsPagesLoaded: state.thoughtsPagesLoaded + 1
             }
         default: return state
     }
@@ -59,10 +73,12 @@ export default profileReducer
 // AC
 
 export const setProfileAC = (userProfile: any) => { return { type: 'SET-PROFILE', userProfile } }
-export const setSubscribeStatusAC = (subscribe: any) => { return { type: 'SET_SUBSCRIBE', subscribe } }
-export const subscribeAC = (subscribeId: string) => { return { type: 'SUBSCRIBE', subscribeId } } 
+export const setSubscribeStatusAC = (subscribe: any) => { return { type: 'SET-SUBSCRIBE', subscribe } }
+export const subscribeAC = (subscribeId: string) => { return { type: 'SUBSCRIBE', subscribeId } }
 export const unsubscribeAC = () => { return { type: 'UNSUBSCRIBE' } }
-export const setUserSubscribesAC = (subscribesPage: any, totalUserSubscribes: number) => { return { type: 'SET_USER_SUBSCRIBES', subscribesPage, totalUserSubscribes } }
+export const setUserSubscribesAC = (subscribesPage: any, totalUserSubscribes: number) => { return { type: 'SET-USER-SUBSCRIBES', subscribesPage, totalUserSubscribes } }
+export const loadFirstThoughtsPageAC = (thoughtsPage: any) => { return { type: 'LOAD-FIRST-THOUGHTS-PAGE', thoughtsPage } }
+export const setProfileThoughtsAC = (thoughtsPage: any) => { return { type: 'SET-THOUGHTS-PAGE', thoughtsPage } }
 
 // Thunk
 
@@ -99,5 +115,21 @@ export const unsubscribeTK = (subscribe_id: string) => {
         const deleteSubRes = await subscribesAPI.deleteSubscribe(subscribe_id)
         if ('error' in deleteSubRes) return null
         else dispatch(unsubscribeAC())
+    }
+}
+
+export const loadFirstThoughtsPageTK = (user_id: string, page_size: number) => {
+    return async (dispatch: any) => {
+        const getThoughtsPageRes = await thoughtsAPI.getThoughtsPage(user_id, 1, page_size)
+        if ('error' in getThoughtsPageRes) return null
+        else dispatch(loadFirstThoughtsPageAC(getThoughtsPageRes.data.profileThoughtsPage))
+    }
+}
+
+export const loadNextThoughtsPageTK = (user_id: string, page_number: number, page_size: number) => {
+    return async (dispatch: any) => {
+        const getThoughtsPageRes = await thoughtsAPI.getThoughtsPage(user_id, page_number, page_size)
+        if ('error' in getThoughtsPageRes) return null
+        else dispatch(setProfileThoughtsAC(getThoughtsPageRes.data.profileThoughtsPage))
     }
 }
